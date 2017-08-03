90a9b0fb84749c66f4c471a4bfdff6b47fcdeed2
Alexandr Em <alexandr.em@chulakov.ru>
2017-08-03 11:57:49 +0300

    Подключение поведения для работы с изображениями

--- backend/config/main.php
+++ backend/config/main.php
@@ -24,7 +24,17 @@ return [
             ],
             'layout' => 'left-menu',
             'mainLayout' => '@app/views/layouts/main.php',
-        ]
+        ],
+        'yii2images' => [
+            'class' => 'rico\yii2images\Module',
+            //be sure, that permissions ok
+            //if you cant avoid permission errors you have to create "images" folder in web root manually and set 777 permissions
+            'imagesStorePath' => '@public/upload/images/store', //path to origin images
+            'imagesCachePath' => '@public/upload/images/cache', //path to resized copies
+            'graphicsLibrary' => 'GD', //but really its better to use 'Imagick'
+            'placeHolderPath' => '@public/upload/images/placeHolder.png', // if you want to get placeholder when image not exists, string will be processed by Yii::getAlias
+            'imageCompressionQuality' => 100, // Optional. Default value is 85.
+        ],
     ],
     'as access' => [
         'class' => 'mdm\admin\components\AccessControl',
